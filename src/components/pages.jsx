bc061f1c5228171c21e1ae3f351e89186b98832e
Tamik <gabaraev.tn@gmail.com>
2022-06-10 18:38:56 +0300

    pagination is hidden when task amount < 6

--- src/components/pages.jsx
+++ src/components/pages.jsx
@@ -2,44 +2,49 @@ import React from "react";
 
 export default function Pages(props) {
   const pageNumbers = [];
-  for (let i = 1; i <= props.pageCount; i++) {
-    pageNumbers.push(i);
-  }
-
-  return (
-    <div className="page_number_container">
-      <button
-        className="btn page_number"
-        onClick={() =>
-          props.paginate(props.curPage - 1)
-        }
-        id="<<"
-        disabled={props.curPage === 1}
-      >
-        {"<<"}
-      </button>
-      {pageNumbers.map((number) => (
+  if (props.pageCount > 1) {
+    for (let i = 1; i <= props.pageCount && props.pageCount > 1; i++) {
+      pageNumbers.push(i);
+    }
+    return (
+      
+        <div className="page_number_container">
         <button
           className="btn page_number"
-          disabled={props.curPage === number}
-          onClick={() => props.paginate(number)}
-          id={number}
+          onClick={() =>
+            props.paginate(props.curPage - 1)
+          }
+          id="<<"
+          disabled={props.curPage === 1}
         >
-          {number}
+          {"<<"}
         </button>
-      ))}
-      <button
-        className="btn page_number"
-        onClick={() =>
-          props.paginate(props.curPage + 1)
-        }
-        id=">>"
-        disabled={
-          pageNumbers.length === 0 || props.curPage === pageNumbers.length
-        }
-      >
-        {">>"}
-      </button>
-    </div>
-  );
+      
+        {pageNumbers.map((number) => (
+          <button
+            className="btn page_number"
+            disabled={props.curPage === number}
+            onClick={() => props.paginate(number)}
+            id={number}
+          >
+            {number}
+          </button>
+        ))}
+        <button
+          className="btn page_number"
+          onClick={() =>
+            props.paginate(props.curPage + 1)
+          }
+          id=">>"
+          disabled={
+            pageNumbers.length === 0 || props.curPage === pageNumbers.length
+          }
+        >
+          {">>"}
+        </button>
+      </div>
+    );
+  } else {
+    return
+  }
 }
